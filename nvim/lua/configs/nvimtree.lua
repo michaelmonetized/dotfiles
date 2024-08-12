e0fe401f678db23956e61bbac60a40e0308ff393
Michael Monetized <michaelmonetized@gmail.com>
2024-08-12 07:11:13 -0400

    aerospace nvim

---
+++ nvim/lua/configs/nvimtree.lua
@@ -0,0 +1,19 @@
+local options = {
+  git = {
+    enable = true,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+  view = {
+    side = "right",
+  },
+}
+
+return options
+
