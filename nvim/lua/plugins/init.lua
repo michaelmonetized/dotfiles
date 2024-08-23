8266c595609a3ba744b37c864ff66e1305961024
Michael Monetized <michaelmonetized@gmail.com>
2024-08-23 17:44:09 -0400

    welcome

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,20 @@
 return {
   {
+    "supermaven-inc/supermaven-nvim",
+    lazy = false,
+    config = function()
+      require("supermaven-nvim").setup {
+        keymaps = {
+          accept_word = "<c-a>",
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+        },
+      }
+    end,
+  },
+  {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
+    event = "BufWritePre",
     opts = require "configs.conform",
   },
   {
@@ -10,19 +23,7 @@ return {
       require "configs.lspconfig"
     end,
   },
-  {
-    "supermaven-inc/supermaven-nvim",
-    lazy = false,
-    config = function()
-      require("supermaven-nvim").setup {
-        keymaps = {
-          accept_suggestion = "<Tab>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-.>",
-        },
-      }
-    end,
-  },
+
   {
     "xiyaowong/transparent.nvim",
     lazy = false,
